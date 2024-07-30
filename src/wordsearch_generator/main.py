35067b370a04c0b0c89ab012ad65a4d33d5979a0
DanielBaudryYounup <d.baudry@younup.fr>
2024-07-30 16:41:45 +0200

    step

--- src/wordsearch_generator/main.py
+++ src/wordsearch_generator/main.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Tuple
 
 class WordDoesNotFitException(Exception):
     pass
@@ -8,33 +8,31 @@ class GridGenerator:
         return [['' for _ in range(width)] for _ in range(length)]
 
     def insert_word_in_grid_horizontally(self, word: str, grid: List[List[str]]) -> None:
-        if len(word) > len(grid[0]):
-            raise WordDoesNotFitException("Word does not fit in the grid")
-        else:
-            for i, char in enumerate(word):
-                grid[0][i] = char
-            for row in grid:
-                for i in range(len(word), len(row)):
-                    row[i] = ''
+        self.insert_word_in_grid(word, grid, (0, 0))
 
     def insert_word_in_grid_vertically(self, word: str, grid: List[List[str]]) -> None:
-        if len(word) > len(grid):
-            raise WordDoesNotFitException("Word does not fit in the grid")
-        else:
-            for i, char in enumerate(word):
-                grid[i][0] = char
-            for col in range(1, len(grid[0])):
-                for row in grid:
-                    row[col] = ''
+        self.insert_word_in_grid(word, grid, (0, 0), vertical=True)
 
     def insert_word_in_grid_diagonally(self, word: str, grid: List[List[str]]) -> None:
-        if len(word) > min(len(grid), len(grid[0])):
-            raise WordDoesNotFitException("Word does not fit in the grid")
+        self.insert_word_in_grid(word, grid, (0, 0), diagonal=True)
+
+    def insert_word_in_grid(self, word: str, grid: List[List[str]], start_coords: Tuple[int, int], vertical=False, diagonal=False) -> None:
+        start_row, start_col = start_coords
+        if vertical:
+            grid_length = len(grid)
+        elif diagonal:
+            grid_length = min(len(grid), len(grid[0]))
         else:
-            for i, char in enumerate(word):
-                grid[i][i] = char
-            for i in range(len(word)):
-                for j in range(len(word), len(grid[0])):
-                    grid[i][j] = ''
-                for j in range(len(word), len(grid)):
-                    grid[j][i] = ''
+            grid_length = len(grid[0])
+
+        if len(word) > grid_length:
+            raise WordDoesNotFitException("Word does not fit in the grid")
+
+        for i, char in enumerate(word):
+            grid[start_row + i][start_col + i] = char
+
+        for i in range(len(word)):
+            for j in range(len(word), len(grid[0])):
+                grid[start_row + i][start_col + j] = ''
+            for j in range(len(word), len(grid)):
+                grid[start_row + j][start_col + i] = ''
