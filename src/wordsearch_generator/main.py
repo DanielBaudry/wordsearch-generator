3e1481d682b38202205a9c5c0c783a916aa11419
DanielBaudryYounup <d.baudry@younup.fr>
2024-07-30 16:13:13 +0200

    step

--- src/wordsearch_generator/main.py
+++ src/wordsearch_generator/main.py
@@ -1,3 +1,8 @@
 class GridGenerator:
     def generate_grid(self, width, length):
         return [[''] * width for _ in range(length)]
+
+    def insert_word_in_grid(self, word, grid):
+        # Insert the word in the first row of the grid
+        for i, char in enumerate(word):
+            grid[0][i] = char
