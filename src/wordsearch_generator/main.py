bab62a1cfb796b9540752dcabd7c0c2a93b8caaa
DanielBaudryYounup <d.baudry@younup.fr>
2024-11-26 14:14:59 +0100

    Manual Dev

--- src/wordsearch_generator/main.py
+++ src/wordsearch_generator/main.py
@@ -1,47 +1,140 @@
-from pprint import pprint
-from typing import List, Tuple
+from enum import Enum
+import random
 
-class WordDoesNotFitException(Exception):
-    pass
 
-class GridGenerator:
-    def generate_grid(self, width: int, length: int) -> List[List[str]]:
-        return [['' for _ in range(width)] for _ in range(length)]
+class WordDirection(Enum):
+    HORIZONTAL = "HORIZONTAL"
+    VERTICAL = "VERTICAL"
+    DIAGONAL = "DIAGONAL"
 
-    def insert_word_in_grid_horizontally(self, word: str, grid: List[List[str]], start_coords: Tuple[int, int]) -> None:
-        self.insert_word_in_grid(word, grid, start_coords)
+class Grid:
+    words: list[str]
+    width: int
+    height: int
 
-    def insert_word_in_grid_vertically(self, word: str, grid: List[List[str]], start_coords: Tuple[int, int]) -> None:
-        self.insert_word_in_grid(word, grid, start_coords, vertical=True)
+    def __init__(self, width: int, height: int):
+        self.width = width
+        self.height = height
+        self.words = []
+        self.grid = [["" for _ in range(width)] for _ in range(height)]
 
-    def insert_word_in_grid_diagonally(self, word: str, grid: List[List[str]], start_coords: Tuple[int, int]) -> None:
-        self.insert_word_in_grid(word, grid, start_coords, diagonal=True)
+    def is_empty_cell(self, row: int, col: int) -> bool:
+        return self.grid[row][col] == ""
+
+class WordPlacer:
+    def __init__(self, grid: Grid):
+        self.grid = grid
+
+    def place_words_in_grid(self, words: list[str]) -> bool:
+        for word in words:
+            word_is_placed = False
+            counter = 0
+            while not word_is_placed:
+                word_is_placed = self.place_word_in_grid(word)
+                if counter > 10000:
+                    raise Exception('Word placement failed for word: {word}')
+        return True
+
+    def place_word_in_grid(self, word: str) -> bool:
+        direction = self.get_random_direction()
+        start_row, start_col = self.get_random_empty_cell()
+        print(f"Placing word '{word}' in direction {direction}")
+
+        if direction == WordDirection.HORIZONTAL:
+            if self.can_place_word_horizontally(start_row, start_col, word):
+                self.place_word_horizontally(start_row, start_col, word)
+                return True
+            return False
+        elif direction == WordDirection.VERTICAL:
+            if self.can_place_word_vertically(start_row, start_col, word):
+                self.place_word_vertically(start_row, start_col, word)
+                return True
+            return False
+        elif direction == WordDirection.DIAGONAL:
+            if self.can_place_word_diagonally(start_row, start_col, word):
+                self.place_word_diagonally(start_row, start_col, word)
+                return True
+            return False
     
-    def insert_word_in_grid(self, word: str, grid: List[List[str]], start_coords: Tuple[int, int], vertical=False, diagonal=False) -> None:
-        start_row, start_col = start_coords
-        if vertical:
-            grid_length = len(grid)
-        elif diagonal:
-            grid_length = min(len(grid), len(grid[0]))
-        else:
-            grid_length = len(grid[0])
+    def get_random_direction(self) -> WordDirection:
+        return random.choice([WordDirection.HORIZONTAL, WordDirection.VERTICAL, WordDirection.DIAGONAL])
+        
+    def get_random_empty_cell(self) -> tuple[int, int]:
+        while True:
+            row = random.randint(0, self.grid.height - 1)
+            col = random.randint(0, self.grid.width - 1)
+            if self.grid.is_empty_cell(row, col):
+                return row, col
 
-        if len(word) > grid_length:
-            raise WordDoesNotFitException("Word does not fit in the grid")
+    def can_place_word_horizontally(self, row: int, col: int, word: str) -> bool:
+        if col + len(word) > self.grid.width:
+            return False
 
-        for i, char in enumerate(word):
-            if vertical:
-                if grid[start_row + i][start_col] != '' and grid[start_row + i][start_col] != char:
-                    raise WordDoesNotFitException("Word overlaps with existing letters in the grid")
-                grid[start_row + i][start_col] = char
-            elif diagonal:
-                if grid[start_row + i][start_col + i] != '' and grid[start_row + i][start_col + i] != char:
-                    raise WordDoesNotFitException("Word overlaps with existing letters in the grid")
-                grid[start_row + i][start_col + i] = char
-            else:
-                if grid[start_row][start_col + i] != '' and grid[start_row][start_col + i] != char:
-                    raise WordDoesNotFitException("Word overlaps with existing letters in the grid")
-                grid[start_row][start_col + i] = char    
+        for index, letter in enumerate(word):
+            if self.grid.grid[row][col + index]!= "" and self.grid.grid[row][col + index]!= letter:
+                return False
+        return True
+    
+    def can_place_word_vertically(self, row: int, col: int, word: str) -> bool:
+        if row + len(word) > self.grid.height:
+            return False
+        
+        for index, letter in enumerate(word):
+            if self.grid.grid[row + index][col]!= "" and self.grid.grid[row + index][col]!= letter:
+                return False
+            
+        return True
+    
+    def can_place_word_diagonally(self, row: int, col: int, word: str) -> bool:
+        if col + len(word) > self.grid.width or row + len(word) > self.grid.height:
+            return False
+        
+        for index, letter in enumerate(word):
+            if self.grid.grid[row + index][col + index]!= "" and self.grid.grid[row + index][col + index]!= letter:
+                return False
+            
+        return True
+    
+    def place_word_horizontally(self, row: int, col: int, word: str):
+        for i in range(len(word)):
+            self.grid.grid[row][col + i] = word[i]
+        self.grid.words.append(word)
 
-    def print_grid(self, grid: List[List[str]]) -> None:
-        pprint(grid)
+    def place_word_vertically(self, row: int, col: int, word: str):
+        for i in range(len(word)):
+            self.grid.grid[row + i][col] = word[i]
+        self.grid.words.append(word)
+
+    def place_word_diagonally(self, row: int, col: int, word: str):
+        for i in range(len(word)):
+            self.grid.grid[row + i][col + i] = word[i]
+        self.grid.words.append(word)
+
+class WordSearchGenerator:
+    def generate_word_search(self, width: int, height: int, words: list[str]) -> Grid:
+        grid = Grid(width, height)
+        placer = WordPlacer(grid)
+        placer.place_words_in_grid(words)
+        grid = self.fill_grid_with_random_letters(grid)
+        return grid
+
+    def fill_grid_with_random_letters(self, grid: Grid) -> Grid:
+        random_letters = ""
+        for row in range(grid.height):
+            for col in range(grid.width):
+                if grid.grid[row][col] == "":
+                    random_letter = random.choice("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+                    grid.grid[row][col] = random_letter
+                    random_letters += random_letter
+        print(f"Random letters generated: {random_letters} \n")
+        return grid
+
+# Example usage
+
+word_search_generator = WordSearchGenerator()
+words = ["APPLE", "BANANA", "ORANGE", "GRAPE", "LEMON", "STRAWBERRY", "BLUEBERRY", "CHERRY"]
+grid = word_search_generator.generate_word_search(10, 10, words)
+for row in grid.grid:
+    print("".join(f"[{row or ' '}]"))
+
+print(grid.words)
